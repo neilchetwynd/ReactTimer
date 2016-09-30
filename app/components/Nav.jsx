37e09f7beeca323587706f65b8e8f872f397c9eb
Neil Chetwynd <Neil Chetwynd>
2016-09-30 23:42:28 +0100

    initial commit

---
+++ app/components/Nav.jsx
@@ -0,0 +1,27 @@
+var React = require('react');
+var {Link, IndexLink} = require('react-router');
+
+var Nav = () => {
+  return (
+    <div className="top-bar">
+        <div className="top-bar-left">
+          <ul className="menu">
+            <li className="menu-text">React Timer App</li>
+            <li>
+                <IndexLink to="/" activeClassName="active">Timer</IndexLink>
+            </li>
+            <li>
+              <Link to="/" activeClassName="active">Countdown</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="top-bar-right">
+          <ul className="menu">
+            <li className="menu-text">Created by <a href="http://www.neilchetwynd.com" target="_blank">Neil Chetwynd</a></li>
+           </ul>
+         </div>
+      </div>
+  );
+};
+
+module.exports = Nav;
